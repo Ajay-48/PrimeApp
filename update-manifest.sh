4e4f2d1b28a5353750845db32640d0fd7ec0bab5
Ajay-48 <165189310+Ajay-48@users.noreply.github.com>
2025-03-06 20:25:25 +0530

    Create update-manifest.sh

---
+++ update-manifest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define variables
+DEPLOYMENT_FILE="deployment.yaml"     # Path to your deployment YAML file
+IMAGE_NAME="ajay048/primeapp"           # Your Docker image name (without the tag)
+
+# Update the image tag in the deployment file using sed
+sed -i "s|image: ${IMAGE_NAME}:.*|image: ${IMAGE_NAME}:${BUILD_NUMBER}|" "$DEPLOYMENT_FILE"
+
+
+# Check if the operation was successful
+if [ $? -eq 0 ]; then
+  echo "Successfully updated the image tag to ${BUILD_NUMBER} in the deployment file."
+else
+  echo "Failed to update the image tag."
+  exit 1
+fi
